d3f9938075656212993ed779c02ab3ea9aad1930
Md. Amirul Islam <amirulislam.office28@gmail.com>
2026-06-30 02:53:46 +0600

    feat: add Vercel configuration files and update CORS settings in index.js

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://boimohol-client-e9bw.vercel.app', 'http://localhost:3000'],
+  credentials: true,
+}));
 app.use(express.json());
 
 // ── Auth middleware ────────────────────────────────────────────────────────────
